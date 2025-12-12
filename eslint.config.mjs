b01d5a930813caee7bf537b30546da0dec249809
iKronyx <gayashan.p@hastec.co>
2025-12-12 15:18:32 +0530

    upd: eslint allow let

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ const eslintConfig = [
   {
     rules: {
       // Disable "Unexpected any" error
+      "prefer-const": "off",
       "@typescript-eslint/no-explicit-any": "off",
     },
     ignores: [
